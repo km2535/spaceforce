5c1aa8450cc2183a40b454ac0749b68bdaa422b0
km2535 <km2535@naver.com>
2021-12-10 23:12:28 +0900

    ver2.0

---
+++ js/toggle.js
@@ -0,0 +1,8 @@
+let menutoggle = document.querySelector('.openMOgnb');
+    menutoggle.onclick = function(){
+           menutoggle.classList.toggle('active');
+           navtoggle.classList.toggle('active');
+           headtoggle.classList.toggle('active');
+        }
+let navtoggle = document.querySelector('.gnb')
+let headtoggle = document.querySelector('header')
